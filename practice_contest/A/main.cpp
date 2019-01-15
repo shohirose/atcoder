afbdae04b96d897e2637896d7f8e4a59a6830bfc
Sho Hirose <sho.hirose@utexas.edu>
2019-01-14 20:24:42 -0600

    feat: problem A in practice contest.

---
+++ practice_contest/A/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string>
+
+int main() {
+  double a, b, c;
+  std::cin >> a;
+  std::cin >> b >> c;
+
+  std::string s;
+  std::cin >> s;
+
+  std::cout << a + b + c << ' ' << s << std::endl;
+}
