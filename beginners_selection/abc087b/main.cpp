bac51e089630c899e2e88c0af6bc49366509ba84
Sho Hirose <sho.hirose@utexas.edu>
2019-01-15 00:54:31 -0600

    refactor: simplify inner loops.

--- beginners_selection/abc087b/main.cpp
+++ beginners_selection/abc087b/main.cpp
@@ -1,22 +1,15 @@
 #include <iostream>
 
 int main() {
-  int n500, n100, n50, x;
-  std::cin >> n500;
-  std::cin >> n100;
-  std::cin >> n50;
-  std::cin >> x;
+  int a, b, c, x;
+  std::cin >> a >> b >> c >> x;
 
   int count = 0;
-  for (int i = 0; i <= n500; ++i) {
-    for (int j = 0; j <= n100; ++j) {
-      for (int k = 0; k <= n50; ++k) {
-        if (i * 500 + j * 100 + k * 50 == x) {
-          ++count;
-        }
-      }
+  for (int i = 0; i <= a; ++i) {
+    for (int j = 0; j <= b; ++j) {
+      const auto rsd = x - i * 500 - j * 100;
+      if (rsd >= 0 && rsd / 50 <= c) ++count;
     }
   }
-
   std::cout << count << std::endl;
 }
