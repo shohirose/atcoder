a39e3d62eb024e8f228c72a649dc0f1872dbe569
Sho Hirose <sho.hirose@gmail.com>
2019-02-05 00:10:10 -0600

    refactor: using <algorithm>.

--- beginners_selection/abc081b/main.cpp
+++ beginners_selection/abc081b/main.cpp
@@ -1,36 +1,27 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-inline bool is_even(int i) { return i % 2 == 0; }
+bool is_all_even(const std::vector<int>& values) {
+  auto it = std::find_if(values.begin(), values.end(), [](const auto v){
+    return v % 2 != 0;});
+  return it == values.end();
+}
 
 int main() {
   int n;
   std::cin >> n;
 
-  std::vector<int> a(n);
-  for (auto&& ai : a) {
-    std::cin >> ai;
+  std::vector<int> values(n);
+  for (auto&& v : values) {
+    std::cin >> v;
   }
 
   int count = 0;
 
-  while (true) {
-    bool finish = false;
-    for (auto&& ai : a) {
-      if (!is_even(ai)) {
-        finish = true;
-        break;
-      }
-    }
-
-    if (finish) {
-      break;
-    } else {
-      for (auto&& ai : a) {
-        ai /= 2;
-      }
-      ++count;
-    }
+  while (is_all_even(values)) {
+    std::for_each(values.begin(), values.end(), [](auto& v) { v /= 2; });
+    ++count;
   };
 
   std::cout << count << std::endl;
