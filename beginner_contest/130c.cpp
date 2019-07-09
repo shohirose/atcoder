501479183c62fb57951eafba85d091072fa3f6e3
Sho Hirose <sho.hirose@japex.co.jp>
2019-07-09 12:40:22 +0900

    style: small modification in beginner contest 130C.

--- beginner_contest/130c.cpp
+++ beginner_contest/130c.cpp
@@ -1,9 +1,9 @@
-#include <algorithm>
 #include <iostream>
 
 int main() {
   size_t w, h, x, y;
   std::cin >> w >> h >> x >> y;
   const auto is_center = (2 * x == w && 2 * y == h);
-  std::cout << double(w * h) / 2.0 << ' ' << (is_center ? 1 : 0) << std::endl;
+  const auto rect_area = double(w * h);
+  std::cout << rect_area / 2.0 << ' ' << (is_center ? 1 : 0) << std::endl;
 }
