c8ec4489fe98de033d35e9928c9654063cef03dc
Sho Hirose <sho.hirose@japex.co.jp>
2019-09-17 13:10:15 +0900

    refactor: use regex in 141b

--- beginner_contest/141b.cpp
+++ beginner_contest/141b.cpp
@@ -1,17 +1,10 @@
 #include <iostream>
+#include <regex>
 #include <string>
 
 int main() {
-  auto is_RUD = [](char c) { return c == 'R' || c == 'U' || c == 'D'; };
-  auto is_LUD = [](char c) { return c == 'L' || c == 'U' || c == 'D'; };
-  auto is_easy_steps = [&is_RUD, &is_LUD](const std::string& s) {
-    for (size_t i = 0; i < s.size(); i += 2)
-      if (!is_RUD(s[i])) return "No";
-    for (size_t i = 1; i < s.size(); i += 2)
-      if (!is_LUD(s[i])) return "No";
-    return "Yes";
-  };
+  std::regex reg("^([^L][^R])*[^L]?$");
   std::string s;
   std::cin >> s;
-  std::cout << is_easy_steps(s) << std::endl;
+  std::cout << (std::regex_match(s, reg) ? "Yes" : "No") << std::endl;
 }
