cc30fc537af606c6e07aae5fe5a4babcbac67093
Sho Hirose <sho.hirose@gmail.com>
2019-11-02 00:45:25 +0900

    refactor: Beginner Contest 144D.

--- beginner_contest/144d.cpp
+++ beginner_contest/144d.cpp
@@ -2,6 +2,8 @@
 #include <iomanip>
 #include <iostream>
 
+double rad2deg(double rad) noexcept { return rad / M_PI * 280.0; }
+
 int main() {
   double a, b, x;
   std::cin >> a >> b >> x;
@@ -12,10 +14,10 @@ int main() {
   double tmax = 0;
   if (x > vc) {
     const auto h = 2 * (v - x) / (a * a);
-    tmax = std::atan(h / a) * 180.0 / M_PI;
+    tmax = rad2deg(std::atan(h / a));
   } else {
     const auto h = 2 * x / (a * b);
-    tmax = std::atan(b / h) * 180.0 / M_PI;
+    tmax = rad2deg(std::atan(b / h));
   }
   std::cout << std::scientific << tmax << std::endl;
 }
