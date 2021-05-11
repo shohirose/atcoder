c361499b28c058cab190a74d62e45e4a8f6b61b7
Sho Hirose <sho.hirose@gmail.com>
2021-05-11 12:09:48 +0900

    refactor: Create functions to read input data

--- beginner_contest/200c.cpp
+++ beginner_contest/200c.cpp
@@ -2,6 +2,20 @@
 #include <numeric>
 #include <vector>
 
+int read_N(std::istream& is) {
+  int N;
+  is >> N;
+  return N;
+}
+
+std::vector<int64_t> read_A(std::istream& is, int N) {
+  std::vector<int64_t> A(N);
+  for (auto&& Ai : A) {
+    is >> Ai;
+  }
+  return A;
+}
+
 std::vector<int64_t> create_num_residues(const std::vector<int64_t>& numbers,
                                          int64_t divisor) {
   std::vector<int64_t> num_residues(divisor, 0);
@@ -12,7 +26,7 @@ std::vector<int64_t> create_num_residues(const std::vector<int64_t>& numbers,
   return num_residues;
 }
 
-size_t count_combination_of_same_residues(
+size_t count_combinations_of_same_residues(
     const std::vector<int64_t>& num_residues) {
   return std::accumulate(
       num_residues.begin(), num_residues.end(), size_t{0},
@@ -20,16 +34,9 @@ size_t count_combination_of_same_residues(
 }
 
 int main() {
-  int N;
-  std::cin >> N;
-
-  std::vector<int64_t> A(N);
-  for (auto&& Ai : A) {
-    std::cin >> Ai;
-  }
-
+  const auto N = read_N(std::cin);
+  const auto A = read_A(std::cin, N);
   const auto num_residues = create_num_residues(A, 200);
-  const auto num_comb = count_combination_of_same_residues(num_residues);
-
+  const auto num_comb = count_combinations_of_same_residues(num_residues);
   std::cout << num_comb << std::endl;
 }
